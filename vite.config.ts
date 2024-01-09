34f8b0ca62c9e9a024420ad1b13f4d35429c18f0
Ahmed Wasfy <ahmedwasfy@Ahmeds-MacBook-Pro.local>
2024-01-09 18:22:32 -0500

    Updated to only publish dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
-
+import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts({ include: ['lib'] })],
   build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+    },
     rollupOptions: {
       input: {
         //Entry points for the library, so that it cane be imported as:
         // import Button from 'my-lib/Button'
+        main: resolve(__dirname, 'lib/main.ts'),
         button: resolve(__dirname, 'lib/components/Button/index.tsx'),
         input: resolve(__dirname, 'lib/components/Input/index.tsx'),
+      },
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
       },
       external: ['react', 'react/jsx-runtime'],
     },
